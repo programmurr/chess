be4d8458b983c98733129d4fe730814272db0705
programmurr <conorjm89@gmail.com>
2020-09-11 00:30:21 +0100

    Player class has a name and piece assignment

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
+require_relative 'pieces/black_pieces'
+require_relative 'pieces/white_pieces'
+
 class Player
-  attr_accessor :name
+  attr_accessor :name, :piece
   def initialize(num)
     @name = "Player#{num}"
+    @piece = nil
+  end
+
+  def assign_black_piece
+    self.piece = BlackPiece.new
+  end
+
+  def assign_white_piece
+    self.piece = WhitePiece.new
   end
 end
--- spec/player.rb
+++ spec/player.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../lib/player'
+require_relative '../lib/pieces/black_pieces'
+require_relative '../lib/pieces/white_pieces'
 
 describe Player do
   context '#initialize' do
@@ -16,6 +18,20 @@ describe Player do
       allow(player).to receive(:name) { 'Player2' }
       new_player = Player.new(2)
       expect(new_player.name).to eq player.name
+    end
+  end
+
+  context '#assign_black_piece' do
+    it 'can be assigned the BlackPiece class as a piece attribute' do
+      new_player = Player.new(1)
+      expect { new_player.assign_black_piece }.to change { new_player.piece }.from(nil).to be_instance_of BlackPiece
+    end
+  end
+
+  context '#assign_white_piece' do
+    it 'can be assigned the WhitePiece class as a piece attribute' do
+      new_player = Player.new(1)
+      expect { new_player.assign_white_piece }.to change { new_player.piece }.from(nil).to be_instance_of WhitePiece
     end
   end
 end
