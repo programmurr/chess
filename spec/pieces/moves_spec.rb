b90a9c6bff3ac9be6410f3dfef300e96a3a4508c
programmurr <conorjm89@gmail.com>
2020-09-16 22:04:06 +0100

    Tests updated for Black moves. Now time to REFACTOR!

--- spec/pieces/moves_spec.rb
+++ spec/pieces/moves_spec.rb
@@ -139,5 +139,135 @@ describe Moves do
       move = Moves.new(player, @board)
       expect(move.valid_move?).to eq false
     end
+
+    it 'returns true if the BlackKnight can move to an empty square' do
+      player = double('Player', move: %w[g8 f6], piece: BlackPiece.new)
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq true
+    end
+
+    it 'returns false if BlackKnight tries to move to a square occupied by the same piece color' do
+      player = double('Player', move: %w[g8 e7], piece: BlackPiece.new)
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq false
+    end
+
+    it 'returns false if WhiteKnight tries to move to a square not in its move range' do
+      player = double('Player', move: %w[g8 g6], piece: BlackPiece.new)
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq false
+    end
+
+    it 'returns true if the BlackRook can move to an empty square' do
+      player = double('Player', move: %w[f4 b4], piece: BlackPiece.new)
+      @board.grid[4][5].value = BlackRook.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq true
+    end
+
+    it 'returns false if BlackRook tries to move to a square occupied by the same piece color' do
+      player = double('Player', move: %w[f4 f7], piece: BlackPiece.new)
+      @board.grid[4][5].value = BlackRook.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq false
+    end
+
+    it 'returns false if BlackRook tries to move in an invalid direction' do
+      player = double('Player', move: %w[f4 d6], piece: BlackPiece.new)
+      @board.grid[4][5].value = BlackRook.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq false
+    end
+
+    it 'returns true if the BlackBishop can move to an empty square' do
+      player = double('Player', move: %w[e5 c3], piece: BlackPiece.new)
+      @board.grid[3][4].value = BlackBishop.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq true
+    end
+
+    it 'returns false if BlackBishop tries to move to a square occupied by the same piece color' do
+      player = double('Player', move: %w[d6 e7], piece: BlackPiece.new)
+      @board.grid[2][3].value = BlackBishop.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq false
+    end
+
+    it 'returns false if BlackBishop tries to move in an invalid direction' do
+      player = double('Player', move: %w[f6 a6], piece: BlackPiece.new)
+      @board.grid[2][5].value = BlackBishop.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq false
+    end
+
+    it 'returns true if the BlackQueen can move to an empty square in a vertical direction' do
+      player = double('Player', move: %w[f6 f3], piece: BlackPiece.new)
+      @board.grid[2][5].value = BlackQueen.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq true
+    end
+
+    it 'returns true if the BlackQueen can move to an empty square in a horizontal direction' do
+      player = double('Player', move: %w[d5 a5], piece: BlackPiece.new)
+      @board.grid[3][3].value = BlackQueen.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq true
+    end
+
+    it 'returns true if the BlackQueen can move to an empty square in a diagonal direction' do
+      player = double('Player', move: %w[c4 a6], piece: BlackPiece.new)
+      @board.grid[4][2].value = BlackQueen.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq true
+    end
+
+    it 'returns false if BlackQueen tries to move to a square occupied by the same piece color' do
+      player = double('Player', move: %w[h5 h7], piece: BlackPiece.new)
+      @board.grid[3][7].value = BlackQueen.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq false
+    end
+
+    it 'returns false if BlackQueen tries to move in an invalid direction' do
+      player = double('Player', move: %w[c4 b6], piece: BlackPiece.new)
+      @board.grid[4][2].value = BlackQueen.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq false
+    end
+
+    it 'returns true if the BlackKing can move to an adjascent empty square (vertical)' do
+      player = double('Player', move: %w[e6 e5], piece: BlackPiece.new)
+      @board.grid[2][4].value = BlackKing.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq true
+    end
+
+    it 'returns true if the BlackKing can move to an adjascent empty square (horizontal)' do
+      player = double('Player', move: %w[c6 d6], piece: BlackPiece.new)
+      @board.grid[2][2].value = BlackKing.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq true
+    end
+
+    it 'returns true if the BlackKing can move to an adjascent empty square (diagonal)' do
+      player = double('Player', move: %w[h4 g5], piece: BlackPiece.new)
+      @board.grid[4][7].value = BlackKing.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq true
+    end
+
+    it 'returns false if the BlackKing tries to move to a square occupied by the same piece color' do
+      player = double('Player', move: %w[a6 a7], piece: BlackPiece.new)
+      @board.grid[2][0].value = BlackKing.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq false
+    end
+
+    it 'returns false if the BlackKing tries to make an invalid move' do
+      player = double('Player', move: %w[a6 d3], piece: BlackPiece.new)
+      @board.grid[2][0].value = BlackKing.new
+      move = Moves.new(player, @board)
+      expect(move.valid_move?).to eq false
+    end
   end
 end
